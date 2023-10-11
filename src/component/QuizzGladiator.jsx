8316f26478153c5529cbbe76d7308fb9e27ec2b0
Rémy Leroy <remy.leroy77@hotmail.fr>
2023-10-11 17:43:23 +0200

    Debut reponse Question

--- src/component/QuizzGladiator.jsx
+++ src/component/QuizzGladiator.jsx
@@ -4,34 +4,45 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 const questions = [
   {
-    
-    contenu:'Quel est le réalisateur du film ?',
+
+    contenu: 'Quel est le réalisateur du film ?',
+    reponse: 'oui',
     difficulty: 'Facile',
   },
   {
-    
-    contenu:'Qui a composé la bande originale du film ?',
+
+    contenu: 'Qui a composé la bande originale du film ?',
+    reponse: 'oui',
     difficulty: 'Moyen',
   },
   {
-    
-    contenu:'Dans quelle contrée a lieu la première bataille du film ?',
+
+    contenu: 'Dans quelle contrée a lieu la première bataille du film ?',
+    reponse: 'oui',
     difficulty: 'Difficile',
   },
   {
-    
-    contenu:'Comment se prénomme le serviteur de Maximus ?',
+
+    contenu: 'Comment se prénomme le serviteur de Maximus ?',
+    reponse: 'oui',
     difficulty: 'Moyen',
   },
   {
-    
-    contenu:'Quels objets sont enterrés par Juba après la mort de Maximus ?',
+
+    contenu: 'Quels objets sont enterrés par Juba après la mort de Maximus ?',
+    reponse: 'Non',
     difficulty: 'Difficile',
   },
 ];
 
+
+function valider(e) {
+  e.preventDefault()
+}
+
 const QuizzGladiator = () => {
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [input_reponse, setInputReponse] = useState("")
 
   const handleNextQuestion = () => {
     setActiveQuestionIndex((prevIndex) =>
@@ -44,8 +55,11 @@ const QuizzGladiator = () => {
       prevIndex > 0 ? prevIndex - 1 : questions.length - 1
     );
   };
+
+  let ErrorReponse = input_reponse.includes(questions[activeQuestionIndex].reponse)
+
   return (
-    <div style={{ height: '100vh', margin:'80px auto 30px auto'}}>
+    <div style={{ height: '100vh', margin: '80px auto 30px auto' }}>
       <Typography variant="h4" align="center" gutterBottom>
         Question {activeQuestionIndex + 1}
       </Typography>
@@ -55,18 +69,29 @@ const QuizzGladiator = () => {
       <Typography variant="body1" align="center" gutterBottom>
         {questions[activeQuestionIndex].contenu}
       </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        <input type='text' placeholder='Votre réponse' name='nom_reponse' onChange={(e) => setInputReponse(e.target.value)} />
+
+        {ErrorReponse ?
+          <div>
+            Bonne réponse
+          </div>
+          :
+          <p>Mauvaise réponse</p>
+        }
+      </Typography>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={handlePreviousQuestion} style={{ marginRight:'5px' }} variant="contained" color="primary">
+        <Button onClick={handlePreviousQuestion} style={{ marginRight: '5px' }} variant="contained" color="primary">
           Précédent
         </Button>
         <Button onClick={handleNextQuestion} variant="contained" color="primary">
           Suivant
         </Button>
       </div>
-      <div style={{ textAlign:'center',marginTop:'10px'}}>
-      <Link to="/" className='linkStyle'>
-        Retour à la liste des films
-      </Link>
+      <div style={{ textAlign: 'center', marginTop: '10px' }}>
+        <Link to="/" className='linkStyle'>
+          Retour à la liste des films
+        </Link>
       </div>
     </div>
   );
