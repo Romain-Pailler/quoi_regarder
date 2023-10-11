57a39154b8f86a0454e26f0c37782c4e989e5af0
Rémy Leroy <remy.leroy77@hotmail.fr>
2023-10-11 10:14:04 +0200

    Avis

--- src/component/MoviesDetails.jsx
+++ src/component/MoviesDetails.jsx
@@ -32,12 +32,33 @@ const MovieDetails = () => {
       director: "Sidney Lumet",
       actors: ["Henry Fonda", "Martin Balsam"],
       roles: ["M. Davis, le juré no 8, un architecte", "le juré no 1, un entraîneur de football américain"],
-      rating: 9.10,
-      reviews: {
-        name: "Critique 1",
-        avis: "Un film captivant qui explore la justice et la moralité.",
-        note: 9.10,
-      },
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "12 hommes en colère est un film captivant qui explore la justice et la moralité. La performance de Henry Fonda est remarquable.",
+          note: 9.20,
+        },
+        {
+          name: "Maxime",
+          avis: "Ce film est un chef-d'œuvre du cinéma. Il offre une leçon de suspense et d'émotion.",
+          note: 9.50,
+        },
+        {
+          name: "Isabelle",
+          avis: "Des performances d'acteurs exceptionnelles et un scénario brillant font de ce film un incontournable.",
+          note: 9.30,
+        },
+        {
+          name: "Lucas",
+          avis: "12 hommes en colère est un classique intemporel qui mérite d'être vu et revu. Il soulève des questions importantes.",
+          note: 9.40,
+        },
+        {
+          name: "Anna",
+          avis: "Un film puissant qui suscite la réflexion et l'empathie. C'est un must-see pour les amateurs de drame juridique.",
+          note: 9.10,
+        },
+      ],
     },
     {
       id: 1,
@@ -48,8 +69,33 @@ const MovieDetails = () => {
       director: "Grata Gerwing",
       actors: ["Margot Robbie", "Ryan Gosling"],
       roles: ["Barbie", "Ken"],
-      rating: 8.20,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Barbie est un film enchanteur pour les enfants. Il véhicule des valeurs positives et encourage l'imagination.",
+          note: 8.00,
+        },
+        {
+          name: "Maxime",
+          avis: "Mes enfants adorent ce film. Il offre une évasion vers un monde magique et coloré.",
+          note: 7.50,
+        },
+        {
+          name: "Isabelle",
+          avis: "Barbie est un divertissement léger, parfait pour les jeunes spectateurs. Il encourage la créativité.",
+          note: 7.20,
+        },
+        {
+          name: "Lucas",
+          avis: "Un film qui plaira aux enfants, mais qui peut sembler un peu simple pour les adultes.",
+          note: 6.80,
+        },
+        {
+          name: "Anna",
+          avis: "Barbie ne fait pas partie de mes préférences cinématographiques, mais il a son public cible.",
+          note: 6.50,
+        },
+      ],
     },
     {
       id: 2,
@@ -60,8 +106,33 @@ const MovieDetails = () => {
       director: "Quentin Tarantino",
       actors: ["Jamie Foxx", "Christoph Waltz"],
       roles: ["Django Freeman", "King Schultz"],
-      rating: 8,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Django Unchained est un western audacieux et captivant. Les performances de Jamie Foxx et Christoph Waltz sont exceptionnelles.",
+          note: 8.90,
+        },
+        {
+          name: "Maxime",
+          avis: "Un film qui ne laisse personne indifférent. Christoph Waltz brille, et l'histoire est une réflexion sur l'injustice.",
+          note: 8.70,
+        },
+        {
+          name: "Isabelle",
+          avis: "Quentin Tarantino livre un film intense avec un mélange d'action et de drame. Leonardo DiCaprio est terrifiant.",
+          note: 8.60,
+        },
+        {
+          name: "Lucas",
+          avis: "Django Unchained est un film courageux qui aborde des thèmes délicats. La performance de Samuel L. Jackson est marquante.",
+          note: 8.50,
+        },
+        {
+          name: "Anna",
+          avis: "Le film est visuellement spectaculaire, mais il est également dérangeant. Une expérience cinématographique inoubliable.",
+          note: 8.40,
+        },
+      ],
     },
     {
       id: 3,
@@ -72,8 +143,33 @@ const MovieDetails = () => {
       director: "David Fincher",
       actors: ["Brad Pitt", "Edward Norton"],
       roles: ["Tyler Durden", "le narrateur "],
-      rating: 7.4,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Alice",
+          avis: "Fight Club est un film culte, un commentaire social brillant. Les performances sont inoubliables.",
+          note: 9.20,
+        },
+        {
+          name: "Thomas",
+          avis: "Ce film est une véritable claque visuelle et conceptuelle. Brad Pitt est incroyable en Tyler Durden.",
+          note: 9.00,
+        },
+        {
+          name: "Sophie",
+          avis: "Un film provocateur qui pousse à la réflexion sur la société de consommation. Edward Norton est excellent.",
+          note: 8.70,
+        },
+        {
+          name: "Maxime",
+          avis: "Fight Club est un film sombre et dérangeant, mais c'est ce qui le rend si fascinant. Une réflexion sur l'aliénation.",
+          note: 8.40,
+        },
+        {
+          name: "Laura",
+          avis: "J'ai trouvé le film perturbant et parfois trop violent. Ce n'est pas mon genre de film préféré.",
+          note: 7.50,
+        },
+      ],
     },
     {
       id: 4,
@@ -84,8 +180,33 @@ const MovieDetails = () => {
       director: "Ridley Scott",
       actors: ["Russell Crowe", "Joaquin Phoenix"],
       roles: ["Maximus Decimus Meridius", "Commonde "],
-      rating: 10,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Gladiator est un chef-d'œuvre absolu. L'histoire, la performance de Russell Crowe et les scènes de combat sont exceptionnels.",
+          note: 9.50,
+        },
+        {
+          name: "Lucas",
+          avis: "Ce film est une épopée épique avec une bande-son grandiose. Je le considère comme l'un des meilleurs films de tous les temps.",
+          note: 9.60,
+        },
+        {
+          name: "Anna",
+          avis: "Joaquin Phoenix incarne un méchant mémorable. La cinématographie est à couper le souffle, et l'histoire est captivante.",
+          note: 9.30,
+        },
+        {
+          name: "Thomas",
+          avis: "Gladiator est un chef-d'œuvre du cinéma. Il m'a transporté dans l'Antiquité romaine, et Russell Crowe est exceptionnel.",
+          note: 9.70,
+        },
+        {
+          name: "Emma",
+          avis: "Le film est tout simplement incroyable. Russell Crowe est à son meilleur, et l'intrigue est puissante et émotionnelle.",
+          note: 9.40,
+        },
+      ],
     },
     {
       id: 5,
@@ -96,8 +217,33 @@ const MovieDetails = () => {
       director: "Christopher Nolan",
       actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
       roles: ["Dominic « Dom » Cobb, l'extracteur", "Arthur, l'organisateur"],
-      rating: 8,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Inception est un chef-d'œuvre de la science-fiction. Le scénario complexe est magnifiquement réalisé.",
+          note: 9.20,
+        },
+        {
+          name: "Alexandre",
+          avis: "Christopher Nolan a créé un film visuellement spectaculaire. Leonardo DiCaprio livre une performance exceptionnelle.",
+          note: 9.10,
+        },
+        {
+          name: "Isabelle",
+          avis: "Inception est un voyage dans l'imaginaire captivant. La notion de réalité est constamment remise en question.",
+          note: 8.90,
+        },
+        {
+          name: "Thomas",
+          avis: "Ce film est une énigme fascinante qui incite à la réflexion. La musique de Hans Zimmer est inoubliable.",
+          note: 9.00,
+        },
+        {
+          name: "Laura",
+          avis: "Inception est un film audacieux, mais j'ai parfois eu du mal à suivre l'intrigue complexe.",
+          note: 7.80,
+        },
+      ],
     },
     {
       id: 6,
@@ -108,8 +254,33 @@ const MovieDetails = () => {
       director: "Christopher Nolan",
       actors: ["Hugh Jackaman", "Christian Bale"],
       roles: ["Robert Angier / Gerald Root", "Alfred Borden / Fallon"],
-      rating: 7.1,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Le Prestige est un thriller psychologique brillamment exécuté. Les performances de Jackman et Bale sont incroyables.",
+          note: 9.00,
+        },
+        {
+          name: "Maxime",
+          avis: "Un film captivant avec des rebondissements inattendus. L'obsession des personnages est palpitante.",
+          note: 8.80,
+        },
+        {
+          name: "Isabelle",
+          avis: "Christopher Nolan livre un autre chef-d'œuvre du cinéma. Le Prestige est un puzzle fascinant.",
+          note: 9.20,
+        },
+        {
+          name: "Lucas",
+          avis: "Le Prestige est un film intense qui m'a tenu en haleine du début à la fin. La narration est ingénieuse.",
+          note: 8.90,
+        },
+        {
+          name: "Anna",
+          avis: "J'ai adoré les performances des acteurs, mais j'ai trouvé le film un peu sombre et dérangeant.",
+          note: 7.70,
+        },
+      ],
     },
     {
       id: 7,
@@ -120,8 +291,33 @@ const MovieDetails = () => {
       director: "Christopher Nolan",
       actors: ["Cillian Murphy", "Robert Downey Jr."],
       roles: ["Robert Oppenheimer", "Lewis Strauss"],
-      rating: 8.70,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Oppenheimer est un film fascinant qui explore l'histoire du projet Manhattan. La performance de Cillian Murphy est impressionnante.",
+          note: 8.90,
+        },
+        {
+          name: "Maxime",
+          avis: "Ce film offre un regard percutant sur les enjeux de la Seconde Guerre mondiale et les implications de la bombe atomique. Un incontournable.",
+          note: 9.10,
+        },
+        {
+          name: "Isabelle",
+          avis: "Oppenheimer est un chef-d'œuvre de Christopher Nolan. L'histoire est à la fois historiquement précise et captivante.",
+          note: 9.20,
+        },
+        {
+          name: "Lucas",
+          avis: "Le film nous plonge dans l'intensité de la création de la bombe atomique. C'est une expérience cinématographique mémorable.",
+          note: 8.80,
+        },
+        {
+          name: "Anna",
+          avis: "Un film qui offre un aperçu saisissant de l'histoire. Les performances et la réalisation sont de haut niveau.",
+          note: 9.00,
+        },
+      ],
     },
     {
       id: 8,
@@ -132,8 +328,33 @@ const MovieDetails = () => {
       director: "Jonathan Demme",
       actors: ["Jodie Foster", "Anthony Hopkins"],
       roles: ["Clarice Starling", "Dr Hannibal Lecter"],
-      rating: 8.50,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Le Silence des Agneaux est un thriller psychologique incontournable. Les performances de Jodie Foster et Anthony Hopkins sont magistrales.",
+          note: 9.20,
+        },
+        {
+          name: "Maxime",
+          avis: "Un film captivant avec une atmosphère sombre et tendue. Anthony Hopkins en Hannibal Lecter est terrifiant.",
+          note: 8.90,
+        },
+        {
+          name: "Isabelle",
+          avis: "Le Silence des Agneaux est un chef-d'œuvre du suspense. L'intrigue est palpitante et les personnages sont fascinants.",
+          note: 8.80,
+        },
+        {
+          name: "Lucas",
+          avis: "Ce film est un classique intemporel. Jodie Foster apporte une profondeur à son personnage, et Anthony Hopkins est inoubliable.",
+          note: 9.10,
+        },
+        {
+          name: "Anna",
+          avis: "J'ai trouvé le film très intense, mais il m'a captivé du début à la fin. L'histoire est brillamment racontée.",
+          note: 8.70,
+        },
+      ],
     },
     {
       id: 9,
@@ -144,8 +365,33 @@ const MovieDetails = () => {
       director: "Damien Chazelle",
       actors: ["Miles Teller", "J. K. Simmons"],
       roles: ["Andrew Neiman", "Terence Fletcher"],
-      rating: 8.60,
-      reviews: ["Un film captivant qui explore la justice et la moralité."],
+      reviews: [
+        {
+          name: "Sophie",
+          avis: "Whiplash est un film intense qui explore la poursuite de la perfection artistique. Les performances de Teller et Simmons sont incroyables.",
+          note: 9.20,
+        },
+        {
+          name: "Maxime",
+          avis: "Un film brillant qui met en lumière le prix de l'excellence. J.K. Simmons est exceptionnel en tant que mentor tyrannique.",
+          note: 9.10,
+        },
+        {
+          name: "Isabelle",
+          avis: "Whiplash est une expérience cinématographique puissante. La musique et la tension sont captivantes.",
+          note: 8.90,
+        },
+        {
+          name: "Lucas",
+          avis: "Ce film est un tour de force. Il m'a tenu en haleine, et la finale est époustouflante.",
+          note: 9.00,
+        },
+        {
+          name: "Anna",
+          avis: "J'ai été impressionnée par la performance de J.K. Simmons. Whiplash est un film inoubliable sur l'art et l'ambition.",
+          note: 8.80,
+        },
+      ],
     },
   ];
 
@@ -161,6 +407,16 @@ const MovieDetails = () => {
     );
   }
 
+  // Fonction pour calculer la moyenne des notes
+  function calculateAverageRating(reviews) {
+    if (reviews.length === 0) {
+      return 0;
+    }
+
+    const totalRating = reviews.reduce((acc, review) => acc + review.note, 0);
+    return (totalRating / reviews.length).toFixed(2);
+  }
+
   return (
     <div style={containerStyle}>
       <h2 style={titleStyle}>{movie.title}</h2>
@@ -171,13 +427,21 @@ const MovieDetails = () => {
         <ul style={listStyleType}>
           {movie.actors.map((actor, index) => (
             <li key={index}>
-              {actor} a joué le rôle de {movie.roles[index]}
+              {actor} a joué le rôle de {movie.roles[index]}  
             </li>
           ))}
         </ul>
       </p>
-      <p style={textStyle}>Note : {movie.rating}/10</p>
-      <p style={textStyle}>Avis : {movie.reviews}</p>
+      <p style={textStyle}>Note : {calculateAverageRating(movie.reviews)}/10</p>
+      <p style={textStyle}>Avis :
+        <ul style={listStyleType}>
+          {movie.reviews.map((review, index) => (
+            <li key={index}>
+              <strong>{review.name}</strong> a donné une note de {review.note}/10 - "{review.avis}"
+            </li>
+          ))}
+        </ul>
+      </p>
       <Link to="/" style={linkStyle}>
         Retour à la liste des films
       </Link>
