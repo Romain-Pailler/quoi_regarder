d1d5a7e3ed59ea6f0b9756cc073199b9bfdb05ff
Romain-Pailler <romain.pailler77@gmail.com>
2023-10-11 09:55:53 +0200

    ajout du style dans fichiers css

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -3,15 +3,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import React from 'react';
-
-const cardStyle = {
-    maxWidth: '400px', 
-  };
+import '../style/MovieCard.css';
 function MovieCard(props) {
   const { title, imageUrl } = props;
 
   return (
-    <Card sx={cardStyle}>
+    <Card className="cardStyle">
       <CardMedia
         component="img"
         height="auto"
