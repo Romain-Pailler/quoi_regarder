335bdf1bc23f1340ce5f48863d2584046c443793
Rémy Leroy <remy.leroy77@hotmail.fr>
2023-10-24 10:21:01 +0200

    Qizz

--- src/component/QuizzGladiator.jsx
+++ src/component/QuizzGladiator.jsx
@@ -2,62 +2,69 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-const questions = [
+const reponses = [
   {
-
     contenu: 'Quel est le réalisateur du film ?',
-    reponse: 'oui',
+    reponse: 'Ridley Scott',
     difficulty: 'Facile',
   },
   {
-
     contenu: 'Qui a composé la bande originale du film ?',
-    reponse: 'oui',
+    reponse: 'Hans Zimmer',
     difficulty: 'Moyen',
   },
   {
-
     contenu: 'Dans quelle contrée a lieu la première bataille du film ?',
-    reponse: 'oui',
+    reponse: 'Germanie',
     difficulty: 'Difficile',
   },
   {
-
     contenu: 'Comment se prénomme le serviteur de Maximus ?',
-    reponse: 'oui',
+    reponse: 'Cicéron',
     difficulty: 'Moyen',
   },
   {
-
     contenu: 'Quels objets sont enterrés par Juba après la mort de Maximus ?',
-    reponse: 'Non',
+    reponse: 'Figurines en bois',
     difficulty: 'Difficile',
   },
 ];
 
 
-function valider(e) {
-  e.preventDefault()
-}
-
-const QuizzGladiator = () => {
+function QuizzGladiator() {
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-  const [input_reponse, setInputReponse] = useState("")
+  const [input_reponse, setInputReponse] = useState('');
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
   const handleNextQuestion = () => {
     setActiveQuestionIndex((prevIndex) =>
       prevIndex < questions.length - 1 ? prevIndex + 1 : 0
     );
+    réinitialiser();
   };
 
   const handlePreviousQuestion = () => {
     setActiveQuestionIndex((prevIndex) =>
       prevIndex > 0 ? prevIndex - 1 : questions.length - 1
     );
+    réinitialiser();
   };
 
-  let ErrorReponse = input_reponse.includes(questions[activeQuestionIndex].reponse)
+  const handleValidateAnswer = () => {
+    const isCorrect = input_reponse.toLowerCase() === questions[activeQuestionIndex].reponse.toLowerCase();
+    setIsAnswerCorrect(isCorrect);
 
+    // Réinitialise la valeur de l'input
+    réinitialiser();
+
+    // Système de temps pour effacer le message après un certain délai
+    setTimeout(() => {
+      setIsAnswerCorrect(null);
+    }, 5000); // 5000 ms (5 secondes) avant de réinitialiser le message
+  };
+  const réinitialiser = () => {
+    setInputReponse('');// Réinitialise la valeur de l'input
+  };
   return (
     <div style={{ height: '100vh', margin: '80px auto 30px auto' }}>
       <Typography variant="h4" align="center" gutterBottom>
@@ -70,24 +77,28 @@ const QuizzGladiator = () => {
         {questions[activeQuestionIndex].contenu}
       </Typography>
       <Typography variant="body1" align="center" gutterBottom>
-        <input type='text' placeholder='Votre réponse' name='nom_reponse' onChange={(e) => setInputReponse(e.target.value)} />
-
-        {ErrorReponse ?
-          <div>
-            Bonne réponse
-          </div>
-          :
-          <p>Mauvaise réponse</p>
-        }
+        <input type='text' placeholder='Votre réponse' name='nom_reponse' onChange={(e) => setInputReponse(e.target.value)} value={input_reponse} />
       </Typography>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Button onClick={handlePreviousQuestion} style={{ marginRight: '5px' }} variant="contained" color="primary">
           Précédent
         </Button>
+        <Button onClick={handleValidateAnswer} style={{ marginRight: '5px' }} variant="contained" color="primary">
+          Valider
+        </Button>
         <Button onClick={handleNextQuestion} variant="contained" color="primary">
           Suivant
         </Button>
       </div>
+      {isAnswerCorrect !== null && (
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          {isAnswerCorrect ? (
+            <div>Bonne réponse</div>
+          ) : (
+            <div>Mauvaise réponse</div>
+          )}
+        </div>
+      )}
       <div style={{ textAlign: 'center', marginTop: '10px' }}>
         <Link to="/" className='linkStyle'>
           Retour à la liste des films
--- src/component/QuizzPrestige.jsx
+++ src/component/QuizzPrestige.jsx
@@ -2,51 +2,71 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-const questions = [
+const reponses = [
   {
-    
-    contenu:'Qui est le réalisateur du film ?',
+    contenu: 'Qui est le réalisateur du film ?',
+    reponse: 'Christopher Nolan',
     difficulty: 'Facile',
   },
   {
-    
-    contenu:'Quel acteur jouant dans le prestige est présent dans le plus de films de son réalisateur ?',
+    contenu: 'Quel acteur jouant dans Le Prestige est présent dans le plus de films de son réalisateur ?',
+    reponse: 'Michael Caine',
     difficulty: 'Moyen',
   },
   {
-    
-    contenu:"Quel objet Alfred Borden (Bale) utilise t'il lorsqu'il cherche à détourner l'attention ?",
+    contenu: "Quel objet Alfred Borden (Bale) utilise-t-il lorsqu'il cherche à détourner l'attention ?",
+    reponse: 'Une balle',
     difficulty: 'Difficile',
   },
   {
-    
-    contenu:"Pour quel tour de magie les 2 magiciens se battent t'ils ?",
+    contenu: "Pour quel tour de magie les deux magiciens se battent-ils ?",
+    reponse: "L'homme transporté",
     difficulty: 'Moyen',
   },
   {
-    
-    contenu:'Quel grand scientifique fabrique une machine pour Robert ?',
+    contenu: "Quel grand scientifique fabrique une machine pour Robert ?",
+    reponse: 'Nikola Tesla',
     difficulty: 'Difficile',
   },
 ];
 
+
 const QuizzPrestige = () => {
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [input_reponse, setInputReponse] = useState('');
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
   const handleNextQuestion = () => {
     setActiveQuestionIndex((prevIndex) =>
       prevIndex < questions.length - 1 ? prevIndex + 1 : 0
     );
+    réinitialiser();
   };
 
   const handlePreviousQuestion = () => {
     setActiveQuestionIndex((prevIndex) =>
       prevIndex > 0 ? prevIndex - 1 : questions.length - 1
     );
+    réinitialiser();
   };
 
+  const handleValidateAnswer = () => {
+    const isCorrect = input_reponse.toLowerCase() === questions[activeQuestionIndex].reponse.toLowerCase();
+    setIsAnswerCorrect(isCorrect);
+
+    // Réinitialise la valeur de l'input
+    réinitialiser();
+
+    // Système de temps pour effacer le message après un certain délai
+    setTimeout(() => {
+      setIsAnswerCorrect(null);
+    }, 5000); // 5000 ms (5 secondes) avant de réinitialiser le message
+  };
+  const réinitialiser = () => {
+    setInputReponse('');// Réinitialise la valeur de l'input
+  };
   return (
-    <div style={{ height: '100vh', margin:'80px auto 30px auto'}}>
+    <div style={{ height: '100vh', margin: '80px auto 30px auto' }}>
       <Typography variant="h4" align="center" gutterBottom>
         Question {activeQuestionIndex + 1}
       </Typography>
@@ -56,18 +76,33 @@ const QuizzPrestige = () => {
       <Typography variant="body1" align="center" gutterBottom>
         {questions[activeQuestionIndex].contenu}
       </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        <input type='text' placeholder='Votre réponse' name='nom_reponse' onChange={(e) => setInputReponse(e.target.value)} value={input_reponse} />
+      </Typography>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={handlePreviousQuestion} style={{ marginRight:'5px' }} variant="contained" color="primary">
+        <Button onClick={handlePreviousQuestion} style={{ marginRight: '5px' }} variant="contained" color="primary">
           Précédent
+        </Button>
+        <Button onClick={handleValidateAnswer} style={{ marginRight: '5px' }} variant="contained" color="primary">
+          Valider
         </Button>
         <Button onClick={handleNextQuestion} variant="contained" color="primary">
           Suivant
         </Button>
       </div>
-      <div style={{ textAlign:'center',marginTop:'10px'}}>
-      <Link to="/" className='linkStyle'>
-        Retour à la liste des films
-      </Link>
+      {isAnswerCorrect !== null && (
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          {isAnswerCorrect ? (
+            <div>Bonne réponse</div>
+          ) : (
+            <div>Mauvaise réponse</div>
+          )}
+        </div>
+      )}
+      <div style={{ textAlign: 'center', marginTop: '10px' }}>
+        <Link to="/" className='linkStyle'>
+          Retour à la liste des films
+        </Link>
       </div>
     </div>
   );
